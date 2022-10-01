38a127956b76d76d42e8d028666311d7aa0a24ab
Xen0Xys <red57101@gmail.com>
2022-10-01 16:11:31 +0200

    :sparkles: Start adding AdvancementsManager to manage advancements

--- src/main/java/fr/multimc/api/spigot/ServerApi.java
+++ src/main/java/fr/multimc/api/spigot/ServerApi.java
@@ -1,5 +1,11 @@
 package fr.multimc.api.spigot;
 
+import com.fren_gor.ultimateAdvancementAPI.AdvancementTab;
+import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementDisplay;
+import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementFrameType;
+import fr.multimc.api.commons.advancements.AdvancementType;
+import fr.multimc.api.commons.advancements.AdvancementsManager;
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ServerApi extends JavaPlugin {
@@ -18,7 +24,10 @@ public class ServerApi extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        AdvancementsManager advancementsManager = new AdvancementsManager(this, true);
+        AdvancementDisplay advancementDisplay = advancementsManager.getAdvancementDisplay(Material.DIAMOND, "Test Advancement", AdvancementFrameType.CHALLENGE, 5, 0, new String[]{"Description", ":p"});
+        AdvancementTab tab = advancementsManager.addAdvancementTab("test", true);
+        advancementsManager.addAdvancement(AdvancementType.ROOT, "test", tab, advancementDisplay, "textures/block/cobblestone.png");
     }
 
     public static JavaPlugin getInstance() {
