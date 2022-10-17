04a02913936d10dddb75105188cb9307f4980285
Xen0Xys <red57101@gmail.com>
2022-10-17 19:10:47 +0200

    :rocket: Add custom Lobby

--- src/main/java/fr/multimc/api/spigot/samplecode/teams/CustomInstanceSample.java
+++ src/main/java/fr/multimc/api/spigot/samplecode/teams/CustomInstanceSample.java
@@ -1,6 +1,7 @@
 package fr.multimc.api.spigot.samplecode.teams;
 
 import fr.multimc.api.commons.managers.game.instances.Instance;
+import fr.multimc.api.commons.managers.game.instances.InstanceManager;
 import fr.multimc.api.commons.managers.game.instances.InstanceSettings;
 import fr.multimc.api.commons.managers.teammanager.Team;
 import org.bukkit.Location;
@@ -9,8 +10,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.List;
 
 public class CustomInstanceSample extends Instance {
-    public CustomInstanceSample(JavaPlugin plugin, int instanceId, InstanceSettings settings, Location instanceLocation, List<Team> teams) {
-        super(plugin, instanceId, settings, instanceLocation, teams);
+    public CustomInstanceSample(JavaPlugin plugin, InstanceManager instanceManager, int instanceId, InstanceSettings settings, Location instanceLocation, List<Team> teams) {
+        super(plugin, instanceManager, instanceId, settings, instanceLocation, teams);
         plugin.getLogger().info(String.format("Instance %d created", instanceId));
     }
 
