767b52da90b0b4e8a20aea4ac02be701732e8464
Xen0Xys <red57101@gmail.com>
2022-10-28 16:58:34 +0200

    :ambulance: Fix isPlayerOnInstance (normally)

--- src/main/java/fr/multimc/api/spigot/ServerApi.java
+++ src/main/java/fr/multimc/api/spigot/ServerApi.java
@@ -1,6 +1,7 @@
 package fr.multimc.api.spigot;
 
 import fr.multimc.api.spigot.commands.RelativeToCommand;
+import fr.multimc.api.spigot.samplecode.teams.TeamSampleCode;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings({"unused", "ConstantConditions"})
@@ -21,6 +22,7 @@ public class ServerApi extends JavaPlugin {
     @Override
     public void onEnable() {
         this.getCommand("relativeto").setExecutor(new RelativeToCommand());
+        new TeamSampleCode().run(this);
     }
 
     public static JavaPlugin getInstance() {
