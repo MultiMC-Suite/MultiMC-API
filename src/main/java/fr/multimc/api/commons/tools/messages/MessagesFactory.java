d9c31e2d7323c01c2af17ac107ea1e24943a2cb3
Xen0Xys <red57101@gmail.com>
2022-11-19 15:35:18 +0100

    :art: Change getMessage to getChatMessage in MessagesFactory

--- src/main/java/fr/multimc/api/commons/tools/messages/MessagesFactory.java
+++ src/main/java/fr/multimc/api/commons/tools/messages/MessagesFactory.java
@@ -20,11 +20,11 @@ public class MessagesFactory {
         this.prefix = prefix;
     }
 
-    public Component getMessage(@NotNull MessageType messageType, @NotNull String playerName, @NotNull String message, @Nullable String teamName){
-        return this.getMessage(messageType, new ComponentBuilder(playerName).build(), new ComponentBuilder(message).build(), new ComponentBuilder(teamName).build());
+    public Component getChatMessage(@NotNull MessageType messageType, @NotNull String playerName, @NotNull String message, @Nullable String teamName){
+        return this.getChatMessage(messageType, new ComponentBuilder(playerName).build(), new ComponentBuilder(message).build(), new ComponentBuilder(teamName).build());
     }
 
-    public Component getMessage(@NotNull MessageType messageType, @NotNull Component playerName, @NotNull Component message, @Nullable Component teamName){
+    public Component getChatMessage(@NotNull MessageType messageType, @NotNull Component playerName, @NotNull Component message, @Nullable Component teamName){
         if(teamName == null)
             teamName = Component.empty();
         switch (messageType){
@@ -32,27 +32,27 @@ public class MessagesFactory {
                 return Component.text()
                         .append(MessageType.GAME.getComponent())
                         .append(Component.text(" | ", NamedTextColor.GRAY, TextDecoration.BOLD))
-                        .append(this.getTeamedMessage(message, playerName, teamName))
+                        .append(this.getTeamedMessagePart(message, playerName, teamName))
                         .build();
             }
             case TEAM -> {
                 return Component.text()
                         .append(MessageType.TEAM.getComponent())
                         .append(Component.text(" | ", NamedTextColor.GRAY, TextDecoration.BOLD))
-                        .append(this.getPlayerMessage(message, playerName))
+                        .append(this.getPlayerMessagePart(message, playerName))
                         .build();
             }
             default -> {
                 return Component.text()
                         .append(prefix.decorate(TextDecoration.BOLD))
                         .append(Component.text(" | ", NamedTextColor.GRAY, TextDecoration.BOLD))
-                        .append(this.getPlayerMessage(message, playerName))
+                        .append(this.getPlayerMessagePart(message, playerName))
                         .build();
             }
         }
     }
 
-    private Component getPlayerMessage(Component message, Component playerName){
+    private Component getPlayerMessagePart(Component message, Component playerName){
         return Component.text()
                 .append(playerName)
                 .append(Component.text(" » ", NamedTextColor.GRAY, TextDecoration.BOLD))
@@ -60,11 +60,11 @@ public class MessagesFactory {
                 .build();
     }
 
-    private Component getTeamedMessage(@NotNull Component message, @NotNull Component playerName, @NotNull Component teamName){
+    private Component getTeamedMessagePart(@NotNull Component message, @NotNull Component playerName, @NotNull Component teamName){
         return Component.text()
                 .append(teamName.decorate(TextDecoration.ITALIC))
                 .append(Component.text(" "))
-                .append(this.getPlayerMessage(message, playerName))
+                .append(this.getPlayerMessagePart(message, playerName))
                 .build();
     }
 
