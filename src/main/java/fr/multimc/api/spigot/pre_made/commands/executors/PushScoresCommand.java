7fba0621e3ed119dbf7b8d3529dee32a4189b9a8
Xen0Xys <red57101@gmail.com>
2023-01-28 12:21:57 +0100

    :sparkles: Add a default async command to push scores

---
+++ src/main/java/fr/multimc/api/spigot/pre_made/commands/executors/PushScoresCommand.java
@@ -0,0 +1,34 @@
+package fr.multimc.api.spigot.pre_made.commands.executors;
+
+import fr.multimc.api.spigot.managers.ScoreManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings("unused")
+public class PushScoresCommand implements CommandExecutor {
+
+    private final ScoreManager scoreManager;
+    private final Plugin plugin;
+
+    public PushScoresCommand(ScoreManager scoreManager, Plugin plugin) {
+        this.scoreManager = scoreManager;
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                commandSender.sendMessage("Pushing scores...");
+                scoreManager.pushScore();
+                commandSender.sendMessage("Scores pushed!");
+            }
+        }.runTaskAsynchronously(this.plugin);
+        return false;
+    }
+}
