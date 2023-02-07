ec000cace46daac30f00dee635882fb53a032eeb
Xen0Xys <red57101@gmail.com>
2023-02-07 11:34:02 +0100

    :memo: Add documentation

--- src/main/java/fr/multimc/api/spigot/gui/AbstractComponent.java
+++ src/main/java/fr/multimc/api/spigot/gui/AbstractComponent.java
@@ -4,18 +4,39 @@ import fr.multimc.api.spigot.entities.player.MmcPlayer;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Abstract class for representing a GUI component.
+ *
+ * @author MultiMC
+ */
 public abstract class AbstractComponent {
 
     private final ItemStack itemStack;
 
+    /**
+     * Creates a new {@link AbstractComponent} instance.
+     *
+     * @param itemStack The {@link ItemStack} representing this component.
+     */
     public AbstractComponent(ItemStack itemStack) {
         this.itemStack = itemStack;
     }
 
+    /**
+     * Gets the {@link ItemStack} representing this component.
+     *
+     * @return The {@link ItemStack} representing this component.
+     */
     public ItemStack getItemStack() {
         return itemStack;
     }
 
+    /**
+     * Called when the {@link AbstractComponent} is clicked in the GUI.
+     *
+     * @param gui The GUI that this component is part of.
+     * @param mmcPlayer The {@link MmcPlayer} that clicked this component.
+     */
     public abstract void onClicked(@NotNull final AbstractGui gui, @NotNull final MmcPlayer mmcPlayer);
 
 }
