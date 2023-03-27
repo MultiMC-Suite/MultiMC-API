f4bdf9021d30074f0c2793dd0daa4104634726da
Czekaj Tom <red57101@gmail.com>
2023-03-27 10:09:17 +0200

    :rocket: Add GUI sample code

--- src/main/java/fr/multimc/api/sample/spigot/gui/GuiSampleCode.java
+++ src/main/java/fr/multimc/api/sample/spigot/gui/GuiSampleCode.java
@@ -1,7 +1,11 @@
 package fr.multimc.api.sample.spigot.gui;
 
 import fr.multimc.api.sample.spigot.SampleCode;
+import fr.multimc.api.sample.spigot.gui.basic.SampleGui;
+import fr.multimc.api.sample.spigot.gui.linked.SampleLinkedGui;
 import fr.multimc.api.spigot.entities.player.MmcPlayer;
+import fr.multimc.api.spigot.gui.GuiView;
+import fr.multimc.api.spigot.gui.enums.GuiSize;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,15 +13,17 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
-@SuppressWarnings("DataFlowIssue")
+@SuppressWarnings({"DataFlowIssue", "FieldCanBeLocal", "unused"})
 public class GuiSampleCode implements SampleCode, CommandExecutor {
 
     private SampleGui sampleGui;
+    private SampleLinkedGui sampleLinkedGui;
 
     @Override
     public void run(@NotNull final JavaPlugin plugin) {
         // Create a new gui
         this.sampleGui = new SampleGui(plugin);
+        this.sampleLinkedGui = new SampleLinkedGui(plugin, new GuiView(GuiSize.DOUBLE_CHEST));
         // Register debug command
         plugin.getCommand("debug-mmc").setExecutor(this);
     }
@@ -26,6 +32,7 @@ public class GuiSampleCode implements SampleCode, CommandExecutor {
     public boolean onCommand(@NotNull final CommandSender commandSender, @NotNull final Command command, @NotNull final String s, @NotNull final String[] strings) {
         if(commandSender instanceof Player player)
             this.sampleGui.openInventory(new MmcPlayer(player));
+//            this.sampleGui.openInventory(new MmcPlayer(player));
         return false;
     }
 }
